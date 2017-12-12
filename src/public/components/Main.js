a7fc4b62aa33f89d8723bbd6d2b8e01a4d9c42c3
Chris Pfaff <chrispfaff10@gmail.com>
2017-12-12 15:46:06 -0500

    created Main component

---
+++ src/public/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>House Hunters</h1>
+      </div>
+    );
+  }
+
+}
+
+export default Main;
