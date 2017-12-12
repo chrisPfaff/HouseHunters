e4388b31d5d6c70f108565214147d7a4d06d868a
Chris Pfaff <chrispfaff10@gmail.com>
2017-12-12 16:14:23 -0500

    Branch (#5)
    * added material-ui
    
    * added material-ui

---
+++ src/public/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Navbar, Icon } from 'react-materialize';
+
+class NavBar extends Component {
+
+  render() {
+    return (
+      <Navbar brand='logo' right>
+      	<NavItem href='get-started.html'><Icon>search</Icon></NavItem>
+      	<NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
+      	<NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
+      	<NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
+      </Navbar>
+    );
+  }
+
+}
+
+export default NavBar;
